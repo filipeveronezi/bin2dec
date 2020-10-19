a90ee4078e26b39a09453c0e0a5ce87bd3f8773a
Filipe Veronezi <filipeseidi@hotmail.com>
2020-10-19 09:28:02 -0300

    Ajustando visual. Desenvolvendo lógica de conversão em JS para o botão. Versão 1.0 finalizada.

---
+++ public/scripts/bin2dec.js
@@ -0,0 +1,20 @@
+function bin2Dec(){
+    let bin = document.querySelector('#binary').value;
+    let dec = 0;
+
+    for(let i = bin.length - 1; i >= 0; i--) {
+        if(bin[i] != 0 && bin[i] != 1) {
+            alert("You must type a binary number!");
+            return;
+        }
+        let multiplier = Math.pow(2, i);
+
+        console.log(multiplier);
+
+        dec += bin[(bin.length - 1) - i] * multiplier;
+
+        console.log(dec);
+    }
+    
+    document.querySelector('#result').value = dec.toString();
+}
